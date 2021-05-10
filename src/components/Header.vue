a35b7900bac7b878fcc4ac4d787697b649283f2f
Muhammadreza <mra.akhgari@gmail.com>
2021-05-10 05:28:36 -0700

    Add round button component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,26 @@
 <template>
     <header class="main-header">
         <a href="#" class="main-header__item main-header__logo">فروشگاه</a>
-        <a href="index.html" class="main-header__item main-header__home">صفحه اول</a>
+        <a href="#" class="main-header__item main-header__home">صفحه اول</a>
         <a href="#" class="main-header__item main-header__contact-us">تماس با ما </a>
         <a href="#" class="main-header__item main-header__support">پشتیبانی</a>
         <a href="#" class="main-header__item main-header__products">محصولات</a>
-        <button class="main-header__button">ورود / ثبت‌نام </button>
+        <RoundButton text="ورود / ثبت‌نام" class="main-header__button"/>
     </header>
 </template>
 
 <script>
+import RoundButton from './RoundButton'
+
 export default {
     name: 'Header',
+    components: {
+        RoundButton
+    }
 }
 </script>
 
 <style scoped>
-    header {
-        background-color: blueviolet;
-    }
-
-
 .main-header {
     background-color: #ffff;
     height: 65px;
@@ -39,16 +39,8 @@ export default {
 .main-header__button {
     margin-right: auto; /* for justify self to right side */
     margin-left: 25px;
-    background-color: white;
-    border: 2px solid #FFC70E;
-    text-align: center;
-    padding: 10px 20px;
-    border-radius: 24px
 }
 
-.main-header__button:hover{
-    background-color: #FFC70E;
-}
 
 .main-header__logo {
     margin: 25px;
---
+++ src/components/RoundButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button>{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: 'RoundButton',
+    props: {
+        text: String,
+    }
+}
+</script>
+
+<style scoped>
+    button {
+        background-color: white;
+        border: 2px solid #FFC70E;
+        text-align: center;
+        padding: 10px 20px;
+        border-radius: 24px
+    }
+
+    button:hover {
+        background: #FFC70E;
+    }
+</style>
