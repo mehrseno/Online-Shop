6df46baaefbfbf0b2347f3edd5a8887d5ada7f7d
mehrseno <m.seno@aut.ac.ir>
2021-05-25 11:48:56 +0430

    add LoginForm to vue

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,11 +1,24 @@
 <template>
-  <h2>im login form</h2>  
+  <div>
+    <LoginSubform type="text" name="ایمیل" inputPlaceholder="اه" />
+    <LoginSubform type="text" name="ایمیل" inputPlaceholder="اه" />
+  </div>
 </template>
 
 <script>
 import LoginSubform from "./LoginSubform.vue";
 export default {
-  components: { LoginSubform },
   name: "LoginForm",
+  components: { LoginSubform },
 };
 </script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+   gap: 1.3em; 
+  margin-bottom: 30px; 
+}
+</style>
+
