b36423a24fa610b4755af2148ec7ebf1d800d225
mehrseno <m.seno@aut.ac.ir>
2021-05-25 10:41:17 +0430

    Start vue for Login page

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>im login form</h2>  
+</template>
+
+<script>
+import LoginSubform from "./LoginSubform.vue";
+export default {
+  components: { LoginSubform },
+  name: "LoginForm",
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,31 @@
 <template>
-    <h1>HELLO</h1>
+  <div>
+    <h1>فروشگاه-ورود</h1>
+    <LoginForm />
+    <SubmitButton type="button" class="search-wrapper__submit" submit="ورود" />
+  </div>
 </template>
 
+<script>
+import SubmitButton from "../components/SubmitButton.vue";
+import LoginForm from "../components/LoginForm.vue";
+export default {
+  name: "Login",
+  components: {
+    SubmitButton,
+    LoginForm,
+  },
+};
+</script> -->
+
 <style scoped>
-    h1 {
+    div {
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
         height: 100vh;
+        gap: 30px;
     }
 </style>
+
